d6b5c45e6254f7807cd0a7a9e3330fff27703994
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-03 11:34:16 -0300

    funcao cabecalho

--- jogo.c
+++ jogo.c
@@ -8,11 +8,19 @@ void criar_tabuleiro(char tabuleiro[3][3]){
     }
 }
 
+void cabecalho(){
+    printf("==============================\n");
+    printf("  BEM VINDO AO JOGO DA VELHA\n");
+    printf("==============================\n\n");
+}
+
 int main(){
 
     char tabuleiro[3][3];
 
     criar_tabuleiro(tabuleiro);
+
+    cabecalho();
 
     return 0;
 }
